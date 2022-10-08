0f63372fc18540889512f21ef2d916221dba4628
cpedrohenrique1 <88724009+cpedrohenrique1@users.noreply.github.com>
2022-10-07 23:51:52 -0300

    outro

---
+++ outro.py
@@ -0,0 +1,14 @@
+simnao = input("O mundo é plano?")
+if simnao == "sim":
+    print ("Cuidado onde pisa")
+elif simnao == "mais ou menos":
+    print ("mais ou menos plano? ou mais ou menos reto?\n eis a question")
+else:
+    print ("Voce tem mais de 2 neuronios funcionais, parabéns!!!")
+print (5/3)
+print (5//3)
+print (5 % 3)
+print (5**3)
+x = int(input("Insira a cordenada X:"))
+y = int(input("Insira a cordenada Y:"))
+print ("Sua resolução possui:", x*y, " pixeis")
